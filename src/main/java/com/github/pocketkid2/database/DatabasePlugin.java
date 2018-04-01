bdab24a0534406e634ba6e3cd5e5044d7a982d04
Adam <afranklint16@gmail.com>
2018-04-01 12:42:46 -0700

    Create main class
    Tested and working

---
+++ src/main/java/com/github/pocketkid2/database/DatabasePlugin.java
@@ -0,0 +1,16 @@
+package com.github.pocketkid2.database;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DatabasePlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		getLogger().info("Done!");
+	}
+
+	@Override
+	public void onDisable() {
+		getLogger().info("Done!");
+	}
+}
