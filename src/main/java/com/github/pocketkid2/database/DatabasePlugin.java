7f83fdca2fdb493dce6e74def7ad4bdd2bcca26d
Adam <afranklint16@gmail.com>
2018-04-01 17:08:15 -0700

    Comments and startup code

--- src/main/java/com/github/pocketkid2/database/DatabasePlugin.java
+++ src/main/java/com/github/pocketkid2/database/DatabasePlugin.java
@@ -9,18 +9,43 @@ public class DatabasePlugin extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
+		// On startup, we are not connected
+		active = false;
+		// Create the config file if it doesn't exist
+		saveDefaultConfig();
+		// Initialize the database with the settings in the config
+		Database.initialize(this, new Settings(getConfig()));
+		// Register the command
+		getCommand("database").setExecutor(new DatabaseCommand(this));
+		// We're done
 		getLogger().info("Done!");
 	}
 
 	@Override
 	public void onDisable() {
+		// We're done
 		getLogger().info("Done!");
 	}
 
+	/**
+	 * Returns true if the plugin is enabled/active/ready to work
+	 *
+	 * For the base plugin, this is used to determine if the connection is alive
+	 *
+	 * For extended plugins, use this when deciding if certain plugin features are
+	 * allowed to operate
+	 *
+	 * @return
+	 */
 	public boolean isActive() {
 		return active;
 	}
 
+	/**
+	 * Changes the current activation status as returned by isActive()
+	 *
+	 * @param active
+	 */
 	public void setActive(boolean active) {
 		this.active = active;
 	}
