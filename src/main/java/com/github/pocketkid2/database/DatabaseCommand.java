b2a8438c720eb2caffc5bbb59bff3d5c6394d5ec
Adam <afranklint16@gmail.com>
2018-04-03 17:52:41 -0700

    Refactor command and add some aliases

--- src/main/java/com/github/pocketkid2/database/DatabaseCommand.java
+++ src/main/java/com/github/pocketkid2/database/DatabaseCommand.java
@@ -22,70 +22,96 @@ public class DatabaseCommand implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (args.length == 0) {
-			// Show status/info to user
-			PluginDescriptionFile pdf = plugin.getDescription();
-			sender.sendMessage(pdf.getFullName());
-			sender.sendMessage("Author: " + String.join(", ", pdf.getAuthors()));
-			sender.sendMessage("Current database status: " + (plugin.isOnline() ? (ChatColor.GREEN + "online") : (ChatColor.RED + "offline")));
-			return false;
+			if (label.equalsIgnoreCase("connect")) {
+				connect(sender);
+			} else if (label.equalsIgnoreCase("disconnect")) {
+				disconnect(sender);
+			} else if (label.equalsIgnoreCase("reconnect")) {
+				reconnect(sender);
+			} else {
+				// Show status/info to user
+				PluginDescriptionFile pdf = plugin.getDescription();
+				sender.sendMessage(pdf.getFullName());
+				sender.sendMessage("Author: " + String.join(", ", pdf.getAuthors()));
+				sender.sendMessage("Current database status: " + (plugin.isOnline() ? (ChatColor.GREEN + "online") : (ChatColor.RED + "offline")));
+				return false;
+			}
 		} else if (args.length == 1) {
 			// Read argument
 			if (args[0].equalsIgnoreCase("connect") || args[0].equalsIgnoreCase("con")) {
-				// Check current state
-				if (plugin.isOnline()) {
-					sender.sendMessage("The database is already online!");
-				} else {
-					// Attempt connection
-					Database.connect();
-					// And display the results
-					if (plugin.isOnline()) {
-						sender.sendMessage("Connection successful!");
-					} else {
-						sender.sendMessage("Connection unsuccessful!");
-					}
-				}
+				connect(sender);
 			} else if (args[0].equalsIgnoreCase("disconnect") || args[0].equalsIgnoreCase("discon")) {
-				// Check current state
-				if (!plugin.isOnline()) {
-					sender.sendMessage("The database is already offline!");
-				} else {
-					// Disconnect
-					Database.disconnect();
-					sender.sendMessage("Disconnected!");
-				}
+				disconnect(sender);
 			} else if (args[0].equalsIgnoreCase("reconnect") || args[0].equalsIgnoreCase("recon")) {
-				// Check current state
-				if (!plugin.isOnline()) {
-					sender.sendMessage("The database is offline! Try /" + label + " connect");
-				} else {
-					// Attempt reconnection
-					Database.disconnect();
-					Database.connect();
-					// Read off results
-					if (plugin.isOnline()) {
-						sender.sendMessage("Reconnection successful!");
-					} else {
-						sender.sendMessage("Reconnection unsuccessful!");
-					}
-				}
+				reconnect(sender);
 			} else if (args[0].equalsIgnoreCase("plugins") || args[0].equalsIgnoreCase("pl")) {
-				// Display list of currently registered plugins
-				StringBuilder sb = new StringBuilder();
-				Set<DatabasePlugin> plugins = Database.getRegisteredPlugins();
-				sb.append(String.format("Database Plugins (%d): ", plugins.size()));
-				List<String> names = new ArrayList<String>();
-				for (DatabasePlugin p : plugins) {
-					names.add((p.isOnline() ? (ChatColor.GREEN) : (ChatColor.RED)) + p.getName());
-				}
-				sb.append(String.join(ChatColor.RESET + ", ", names));
-				sender.sendMessage(sb.toString());
+				plugins(sender);
 			} else {
+				sender.sendMessage(ChatColor.RED + "Unknown argument '" + args[0] + "'");
 				return false;
 			}
 		} else {
+			sender.sendMessage(ChatColor.RED + "Too many arguments!");
 			return false;
 		}
 		return true;
+	}
+
+	private void reconnect(CommandSender sender) {
+		// Check current state
+		if (!plugin.isOnline()) {
+			sender.sendMessage("The database is already offline!");
+		} else {
+			// Attempt reconnection
+			Database.disconnect();
+			Database.connect();
+			// Read off results
+			if (plugin.isOnline()) {
+				sender.sendMessage("Reconnection successful!");
+			} else {
+				sender.sendMessage("Reconnection unsuccessful!");
+			}
+		}
+	}
+
+	private void disconnect(CommandSender sender) {
+		// Check current state
+		if (!plugin.isOnline()) {
+			sender.sendMessage("The database is already offline!");
+		} else {
+			// Disconnect
+			Database.disconnect();
+			sender.sendMessage("Disconnected!");
+		}
+	}
+
+	private void connect(CommandSender sender) {
+		// Check current state
+		if (plugin.isOnline()) {
+			sender.sendMessage("The database is already online!");
+		} else {
+			// Attempt connection
+			Database.connect();
+			// And display the results
+			if (plugin.isOnline()) {
+				sender.sendMessage("Connection successful!");
+			} else {
+				sender.sendMessage("Connection unsuccessful!");
+			}
+		}
+	}
+
+	private void plugins(CommandSender sender) {
+		// Display list of currently registered plugins
+		StringBuilder sb = new StringBuilder();
+		Set<DatabasePlugin> plugins = Database.getRegisteredPlugins();
+		sb.append(String.format("Database Plugins (%d): ", plugins.size()));
+		List<String> names = new ArrayList<String>();
+		for (DatabasePlugin p : plugins) {
+			names.add((p.isOnline() ? (ChatColor.GREEN) : (ChatColor.RED)) + p.getName());
+		}
+		sb.append(String.join(ChatColor.RESET + ", ", names));
+		sender.sendMessage(sb.toString());
 	}
 
 }
