98e104c9d873a4395df356efae3b48377ff6aac1
Adam <afranklint16@gmail.com>
2018-04-04 14:41:17 -0700

    Reduce some console spam

--- src/main/java/com/github/pocketkid2/database/ValidationTask.java
+++ src/main/java/com/github/pocketkid2/database/ValidationTask.java
@@ -11,20 +11,32 @@ public class ValidationTask extends BukkitRunnable {
 	public ValidationTask(DatabasePlugin plugin, int timeout) {
 		this.plugin = plugin;
 		this.timeout = timeout;
+		status = true;
 	}
 
 	@Override
 	public void run() {
+		// Only check if the connection is supposedly online
 		if (plugin.isOnline()) {
+			// Call the API method
 			if (Database.validate(timeout)) {
-				plugin.getLogger().info("Connection validation successful!");
+				// Only display the success message once after the last failure
+				if (!status) {
+					plugin.getLogger().info("Connection validation successful!");
+				}
+				// Always set the status to true after a successful validation
 				status = true;
 			} else {
+				// This happens when we thought we were online but we aren't. This is the key
+				// outcome, so always display
 				plugin.getLogger().info("Connection validation failed! The server is offline!");
+				// And always set status to false
 				status = false;
+				// And forcefully make sure we are disconnected
 				Database.disconnect();
 			}
 		} else {
+			// If this is the first check after a disconnect, let the console know
 			if (status) {
 				plugin.getLogger().info("Server is offline, ignoring validation check until server comes back online!");
 				status = false;
